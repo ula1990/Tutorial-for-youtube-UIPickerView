c01a8fc2574c4714d518f66db8e497f08fe90150
Uladzislau Daratsiuk <uladzislaudaratsiuk@uladzislaus-mbp.lan>
2018-08-04 09:46:50 +0200

    Initial Commit

---
+++ NewProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewProject
+//
+//  Created by Uladzislau Daratsiuk on 2018-08-04.
+//  Copyright © 2018 Uladzislau Daratsiuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
